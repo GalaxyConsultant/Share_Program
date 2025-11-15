b163e5d23f4acd9fb6c3a0430c9c186fdc2a94bc
coocu <jxjphgt626@privaterlay.appleid.com>
2025-11-16 01:21:38 +0900

    UMirror

--- UMirror/UMirror/scrcpy-console.bat
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-@echo off
-scrcpy.exe --pause-on-exit=if-error %*
-=======
-@echo off
-scrcpy.exe --pause-on-exit=if-error %*
->>>>>>> 8620fc6 (first commit)
---
+++ UMirror/scrcpy-console.bat
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+@echo off
+scrcpy.exe --pause-on-exit=if-error %*
+=======
+@echo off
+scrcpy.exe --pause-on-exit=if-error %*
+>>>>>>> 8620fc6 (first commit)
